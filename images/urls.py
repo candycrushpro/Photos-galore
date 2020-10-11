be4284418aa222cfe4df0e145ddc0c1692a132a3
candycrushpro <bnilmar54@gmail.com>
2020-10-11 07:45:26 +0300

    adding URL patterns for my views

---
+++ images/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+app_name = 'images'
+
+urlpatterns=[
+    #post views
+    path('',views.image,name='image'),
+    path('search/',views.search_results,name='search_results'),
+    path('detail/(\d+)',views.detail,name='detail'),
+    path('location/(\d+)',views.location,name='location'),
+
+]
