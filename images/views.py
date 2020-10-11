1ef85bf6ff8c90a38547045e68dacb83075d6265
candycrushpro <bnilmar54@gmail.com>
2020-10-11 07:20:59 +0300

    create first view

--- images/views.py
+++ images/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse,Http404
+from .models import Image,Location,Category
 
 # Create your views here.
+
+
+def image(request):
+    image = Image.get_all()
+    return render(request,"all_images/foto.html", {"image":image})  
